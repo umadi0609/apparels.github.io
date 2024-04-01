4bfa4b82c95ae89800a553410c595e8701a2dc4b
urmilashirole <161549377+urmilashirole@users.noreply.github.com>
2024-04-01 13:10:52 +0530

    Update ProductNav.js

--- src/components/ProductNav.js
+++ src/components/ProductNav.js
@@ -10,7 +10,7 @@ const ProductNav = ({filterItem, menuList}) => {
         menuList.map((curCateg)=> {
           return (
             <>
-            <button type='button' className='btn btn-outline-primary' onClick={()=>filterItem(curCateg)}>{curCateg}</button>
+            <button type='button' className='btn btn-outline-primary mt-5 fw-bold' onClick={()=>filterItem(curCateg)}>{curCateg}</button>
             </>
           )
         })
