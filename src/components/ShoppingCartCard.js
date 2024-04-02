a65dae21955c634200dcce671b4a7198676205b9
urmilashirole <161549377+urmilashirole@users.noreply.github.com>
2024-04-02 19:39:59 +0530

    Update ShoppingCartCard.js

--- src/components/ShoppingCartCard.js
+++ src/components/ShoppingCartCard.js
@@ -6,22 +6,22 @@ const ShoppingCartCard = ({ cartItems, removeFromCart, increaseQuantity, decreas
 
 
     return (
-        <div className="card" style={{ width: '300px', position: 'fixed', top: '100px', right: '20px', maxHeight: '80vh', overflowY: 'auto' }}>
-            <div className="card-header">Shopping Cart</div>
+        <div className="card" style={{  top: '100px', overflowY: 'auto', position:'top' }}>
+            <div className="card-header fw-bold">Shopping Cart</div>
             <ul className="list-group list-group-flush">
                 {cartItems.map((item) => (
                     <li className="list-group-item" key={item.id}>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div className="w-100">
+                        <div className="d-flex justify-content-between align-items-center text-muted">
+                            <div className="w-60">
                                 <span className="text-wrap" style={{ fontSize: '0.9rem' }}>{item.name}</span>
-                                <span className="text-muted ml-2"> x {item.quantity}</span>
+                                <span className="text-muted"> x {item.quantity}</span>
                             </div>
                             <div className="d-flex">
                                 <button type="button" className="btn btn-sm btn-danger mx-2" onClick={() => decreaseQuantity(item)}>-</button>
                                 <span className="text-muted">{item.quantity}</span>
                                 <button type="button" className="btn btn-sm btn-success mx-2" onClick={() => increaseQuantity(item)}>+</button>
-                                <button type="button" className="btn btn-sm btn-danger ml-2" onClick={() => removeFromCart(item)} data-toggle="tooltip" data-placement="top" title="Remove">
-                                    <i className="fa fa-trash"></i>
+                                <button type="button" className="btn btn-sm btn-danger ml-2" onClick={() => removeFromCart(item)} data-bs-toggle="tooltip" data-placement="top" title="Remove">
+                                <i className="fa fa-trash"></i>
                                 </button>
                             </div>
                         </div>
